d507c578e12511ec75a861a5183a75447a54fbb2
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-04-15 20:54:12 +0000

    rota cadastro criada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import livros from "./livroRoutes.js"
+import formulario from "../views/cadastroView.js"
 
 const routes = (app) => {
 
     app.get("/", (req, res) => {
         res.status(200).send("Página inicial");
+    })
+    app.get("/cadastro", (req, res) => {
+        res.status(200).send(formulario())
     })
 
     app.use(express.json(), livros);
--- src/views/cadastroView.js
+++ src/views/cadastroView.js
@@ -1,7 +1,41 @@
 
 const view = (info) => {
     return `
-        
+        <html>
+            <form action="/cadastro" method="post">
+                <input type="hidden" name="id" id="id">
+
+                <div>
+                    <label for="titulo">Titulo:</label>
+                    <input type="text" name="titulo" id="titulo" />
+                </div>
+
+                <div>
+                    <label for="editora">Editora:</label>
+                    <input type="text" name="editora" id="editora" />
+                </div>
+
+                <div>
+                    <label for="autor">Autor:</label>
+                    <input type="text" name="autor" id="autor" />
+                </div>
+
+                <div>
+                    <label for="descricao">Descrição</label>
+                    <textarea name="descricao" id="descricao"></textarea>
+                </div>
+
+                <div>
+                    <label for="preco">Preço:</label>
+                    <input type="text" name="preco" id="preco" />
+                </div>
+
+                <div>
+                    <label for="paginas">Páginas:</label>
+                    <input type="number" name="paginas" id="paginas" />
+                </div>
+            </form>
+        </html>
     `
 }
 

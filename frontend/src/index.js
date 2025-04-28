e41924c98007367f2fdc053929a6dbbb600319cb
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-04-28 14:25:36 +0000

    router-dom instalado e rota root posta

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const GlobalStyle = createGlobalStyle`
 
@@ -33,7 +34,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
